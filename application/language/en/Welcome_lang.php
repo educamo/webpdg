dfc43c309d8f39cfd56803d65769b2e655376b6d
C. Eduardo Carrasco <educamo@gmail.com>
2022-09-18 20:52:31 -0400

    se integro plantillas de lenguaje en espanol

---
+++ application/language/en/Welcome_lang.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Idioma ingles de Administracion
+ */
+
+## Etiquetas Generales del modulo de Administracion
+$lang["lang"]                       = "en";
+$lang["descripcion"]                = "Administrative Panel of the e-commerce application.";
+
+$lang["Titulo-Administracion"]      = "Administration";
+$lang["Dashboard"]                  = "Dashboard";
+$lang["configuracion"]              = "Setting";
+$lang["usuarios"]                   = "User";
+
+$lang["realizado"]                  = "Designed by";
+
+
+## Etiquetas del submodulo de Usuarios
+$lang["Titulo-Usuarios"]            = "User";
+$lang["Cuentas-Usuarios"]           = "User Accounts";
+$lang["nuevo"]                      = "New";
+$lang["nuevoUser"]                  = "New User";
+
+$lang["Permisos"]                   = "Permissions"; 
+
+## Etiquetas del submodulo de Usuarios
+$lang["Titulo-Configuracion"]       = "Setting";
+?>  
---
+++ application/language/es/Welcome_lang.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Idioma español de Administracion
+ */
+
+## Etiquetas Generales del sitioweb
+$lang["lang"]                       = "es";
+$lang["description"]                = "SitioWeb de la marca de diseño gráfico cosmo imagine.";
+
+$lang["title"]                      = "Cosmo Imagine - Diseñadora Gráfica";
+
+$lang["author"]                  = "Cesar Carrasco";
+
+
+## Etiquetas del menu
+$lang["navegation"]                 = "Navegación";
+$lang["home"]                       = "Inicio";
+$lang["proyectos"]                  = "Proyectos";
+$lang['recientes']                  = "Recientes";
+$lang["destacados"]                 = "Destacados";
+$lang["blog"]                       = "Blog";
+$lang["apellidoUsuario"]            = "Apellidos";
+$lang["Email"]                      = "E-mail";
+$lang["tlf"]                        = "N° Teléfono";
+$lang["cel"]                        = "N° Celular";
+$lang["Username"]                   = "Nombre de usuario";
+$lang["Password"]                   = "Contraseña";
+
+$lang["Permisos"]                   = "Permisos"; 
+
+## Etiquetas del submodulo de Usuarios
+$lang["Titulo-Configuracion"]       = "Configuración";
+?>  

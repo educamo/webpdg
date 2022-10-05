2e2ca6572e871bab8bded80a9fd2dbd726aa83e1
C. Eduardo Carrasco <educamo@gmail.com>
2022-10-05 10:43:45 -0400

    se inserta la configuracion de keywords para SEO en la BD

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20221004115500;
+$config['migration_version'] = 20221050103000;
 
 /*
 |--------------------------------------------------------------------------

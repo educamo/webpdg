551f33dd74e2e386c2cdbc9cd607dda39246d68e
C. Eduardo Carrasco <educamo@gmail.com>
2022-10-22 03:04:52 -0400

    agregado archivo de lenguaje panel de administracion

--- application/language/es/Admin_lang.php
+++ application/language/es/Admin_lang.php
@@ -14,4 +14,34 @@ $lang['password']                   = "Contraseña";
 $lang["user"]                       = "Correo";
 $lang["Email"]                      = "name@example.com";
 $lang["mensajeErrlogin"]            = "debe ingresar un correo y contraseña validos";
+$lang['perfil']                     = "Perfil";
+$lang['logout']                     = "Cerrar sesión";
+$lang['desea-salir']                = "está seguro que desea salir.";
+$lang['dashboard']                  = "Dashboard";
+$lang['contents']                   = "Contenidos";
+$lang['slider']                     = "Slider";
+$lang['projects']                   = "Proyectos";
+$lang['category']                   = "Categorías";
+$lang['services']                   = "Servicios";
+$lang['admin']                      = "Administración";
+$lang['admin-user']                 = "Usuarios";
+$lang['reg-user']                   = "Registros de Usuarios";
+$lang['new-user']                   = "Nuevo Usuario";
+$lang['config']                     = "Configuración";
+$lang['admin-config']               = "Configuraciones";
+$lang['modules']                    = "Módulos";
+$lang['listModules']                = "Ver Módulos";
+$lang['modifyModules']              = "Modificar Módulo";
+$lang['company']                    = "Datos Empresa";
+$lang['social']                     = "Redes Sociales";
+$lang['configSocial']               = "Administrar";
+$lang['about']                      = "Nosotros";
+$lang['configAbout']                = "Modificar";
+$lang['configContact']              = "Contacto";
+
+
+## Etiquetas generales del panel administrativo
+$lang['cancel']                     = "Cancelar";
+$lang['out']                        = "Salir";
+
 ?>

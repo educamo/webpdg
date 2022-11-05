1d17ab31961404a6aa717be6eee63a87f61cd6f3
C Eduardo Carrasco <educamo@hotmail.com>
2022-11-04 20:18:01 -0400

    se completo list rol, add rol, del rol y modify rol

--- application/views/listRols.php
+++ application/views/listRols.php
@@ -1,72 +1,238 @@
  <!-- Begin Page Content -->
  <div class="container-fluid">
 
-  <?Php
-        if ($this->session->flashdata('status')) :
+   <?Php
+    if ($this->session->flashdata('status')) :
     ?>
 
-    <div class="alert alert-success">
-        <?=$this->session->flashdata('status');?>
+     <div class="alert alert-success">
+       <?= $this->session->flashdata('status'); ?>
 
 
-    </div>
+     </div>
 
-    <?Php
+   <?Php
 
-        header('Refresh: 2');
-        endif;
+      header('Refresh: 2');
+    endif;
     ?>
 
-<!-- Page Heading -->
-<h1 class="h3 mb-2 text-gray-800"><?= lang('title-rols') ?></h1>
+   <!-- Page Heading -->
+   <h1 class="h3 mb-2 text-gray-800"><?= lang('title-rols') ?></h1>
 
-<!-- DataTales Example -->
-<div class="card shadow mb-4">
-  <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary"><?= lang('list-rols') ?></h6>
-  </div>
-  <div class="card-body">
-    <div class="table-responsive">
-      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-        <thead>
-          <tr>
-            <th><?= lang('rolId') ?></th>
-            <th><?= lang('rolName') ?></th>
-            <th><?= lang('activo') ?></th>
-            <th><?= lang('action') ?></th>
-          </tr>
-        </thead>
-    
-        <tbody>
-            <?Php
+   <!-- DataTales Example -->
+   <div class="card shadow mb-4">
+     <div class="card-header py-3">
+       <h6 class="m-0 font-weight-bold text-primary"><?= lang('list-rols') ?></h6>
+     </div>
+     <div class="row">
+       <div class="col-md-12 text-end mr-5 pr-4">
+         <!-- Button trigger modal -->
+         <button type="button" class="btn btn-default btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
+           Nuevo <i class="fa fa-plus-circle"></i>
+         </button>
+       </div>
+     </div>
+     <div class="card-body">
+       <div class="container-fluid">
+         <div class="table-responsive">
+           <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+             <thead>
+               <tr>
+                 <th><?= lang('rolId') ?></th>
+                 <th><?= lang('rolName') ?></th>
+                 <th><?= lang('activo') ?></th>
+                 <th><?= lang('action') ?></th>
+               </tr>
+             </thead>
+
+             <tbody>
+               <?Php
                 foreach ($rols as $rol) {
-                    
-                    if ($rol->activo == 1) {
-                      $activo = 'Si';
-                    }else {
-                      $activo = 'No';
-                    }
-            ?> 
-          <tr>
-            <td><?=$rol->rolId?></td>
-            <td><?=$rol->rolName?></td>
-            <td><?= $activo ?></td>
-            <td class="text-center">
-                <a href="<?= base_url() ?>Admin/actualizarRol/<?=$rol->rolId?>" class="btn btn-warning text-ligth"> <i class="fa fa-edit"></i></a>
-                <a href="<?= base_url() ?>Admin/borrarRol/<?=$rol->rolId?>" class="btn btn-danger text-ligth"> <i class="fa fa-trash"></i></a>
-            </td>
-          </tr>
-          <?Php
-              }
-          ?>
-        </tbody>
-      </table>
-    </div>
-  </div>
-</div>
 
-</div>
-<!-- /.container-fluid -->
+                  if ($rol->activo == 1) {
+                    $activo = 'Si';
+                  } else {
+                    $activo = 'No';
+                  }
+                ?>
+                 <tr>
+                   <td><?= $rol->rolId ?></td>
+                   <td><?= $rol->rolName ?></td>
+                   <td><?= $activo ?></td>
+                   <td class="text-center">
+                     <a href="<?= base_url() ?>Admin/actualizarRol/<?= $rol->rolId ?>" class="btn btn-warning text-ligth"> <i class="fa fa-edit"></i></a>
+                     <button type="button" class="btn btn-danger text-light delete" name="delete" data-id="<?= $rol->rolId ?>"><i class="fa fa-trash"></i></button>
+                   </td>
+                 </tr>
+               <?Php
+                }
+                ?>
+             </tbody>
+           </table>
+         </div>
+       </div>
 
-</div>
-<!-- End of Main Content -->
+
+       <!-- Modal -->
+       <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+         <div class="modal-dialog modal-dialog-centered modal-lg">
+           <div class="modal-content">
+             <div class="modal-header">
+               <h1 class="modal-title fs-5 font-weight-bold text-primary" id="exampleModalLabel"><?= lang('new-rol') ?></h1>
+               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+             </div>
+             <div class="modal-body">
+               <div class="container-fluid">
+                 <div class="row">
+
+                   <form action="#" method="post" id="frmNewrol">
+                     <div class="row">
+
+                       <div class="col-md-6">
+                         <label for="rolId" class="form-label"><?= lang('rolId') ?></label>
+                         <input type="text" class="form-control" id="rolId" name="rolId" minlength="1" maxlength="10" required>
+                       </div>
+                       <div class="col-md-6">
+                         <label for="rolName" class="form-label"><?= lang('rolName') ?></label>
+                         <input type="text" class="form-control" id="rolName" name="rolName" maxlength="100" required>
+                       </div>
+                       <div class="col-md-12 mt-4">
+                         <label for="rolDescription" class="form-label"><?= lang('rolDescription') ?></label>
+                         <textarea class="form-control" name="rolDescription" id="rolDescription" required></textarea>
+                       </div>
+
+                     </div>
+                   </form>
+
+                 </div>
+               </div>
+               <div class="row mt-5">
+                 <div id="mensaje"></div>
+               </div>
+             </div>
+             <div class="modal-footer">
+               <button type="button" class="btn btn-success" id="saveNewrol"><?= lang('save') ?></button>
+               <button type="button" class="btn btn-danger" id="cancelNewrol" data-bs-dismiss="modal"><?= lang('cancel') ?></button>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+
+ </div>
+ <!-- /.container-fluid -->
+
+ </div>
+ <!-- End of Main Content -->
+
+ <script>
+   $(document).ready(function() {
+     var urlbase = "<?= base_url() ?>"
+
+     $('#saveNewrol').click(function(event) {
+       event.preventDefault();
+
+       //creo variable con la url del ajax
+       var urlajax = urlbase + "Admin/insertRol";
+       // Run $.ajax() here
+       // Using the core $.ajax() method
+       $.ajax({
+
+         // The URL for the request
+         url: urlajax,
+
+         // The data to send (will be converted to a query string)
+         data: $('#frmNewrol').serializeArray(),
+
+         // Whether this is a POST or GET request
+         method: "POST",
+
+         dataType: "json",
+
+
+         beforeSend: function(objeto) {
+           var cargando = '<div class="alert alert-info" role="alert"><strong><?= lang("process") ?></strong><div class="spinner-border ms-auto text-primary text-end ml-5" role="status" aria-hidden="true"></div></div>';
+           $("#mensaje").html(cargando);
+         },
+         success: function(r) {
+           $("#mensaje").html('<div class="alert alert-success" role="alert"><strong><?= lang("new") ?></strong></div>');
+           setTimeout("location.reload(true);", 4000);
+           location.href = urlbase + 'Admin/listRols';
+         },
+         error: function(r) {
+           $("#mensaje").html('<div class="alert alert-danger" role="alert"><strong><?= lang("errorSave") ?></strong></div>');
+           setTimeout("location.reload(true);", 3000);
+         }
+       })
+
+     });
+
+     $('#cancelNewrol').click(function() {
+       alertify.error('<?= lang('cancel-operation') ?>');
+     });
+
+
+     /**
+      * funtion en jquery para borrar registros
+      * Descripción: esta función usa un mensaje de confirmación con la librería alertifyjs
+      * si se confirma (ok) procede a eliminar el registro seleccionado por el id para borrar
+      * si se cancela (cancel) solo informa que la operación fue cancelada mediante un mensaje
+      * @author: Cesar Carrasco
+      * @date: 04/11/2022
+      *
+      */
+     $('.delete').click(function(e) {
+       //se evita el evento default del botón
+       e.preventDefault();
+
+       //se asigna el valor del atributo data-id del botón eliminar a la variable id
+       var id = $(this).attr('data-id');
+
+       // se establecen las opciones de la ventana de confirmación con alertifyjs
+       alertify.defaults.transition = "fade";
+       alertify.defaults.theme.ok = "btn btn-primary";
+       alertify.defaults.theme.cancel = "btn btn-secondary";
+       alertify.defaults.theme.input = "form-control"; //esto si existe algún input en el mensaje de confirmación, en este caso no existe
+       alertify.defaults.glossary.title = "<?= lang('confirm') ?>";
+       alertify.defaults.glossary.ok = '<?= lang('yes') ?>';
+       alertify.defaults.glossary.cancel = '<?= lang('no') ?>';
+       //aquí se genera el dialogo de confirmación con alertifyjs
+       alertify.confirm('<?= lang('confirm-message') ?>',
+         function() {
+           //creo variable con la url del ajax
+           var urlajax = urlbase + "Admin/borrarRol";
+           // Run $.ajax() here
+           // Using the core $.ajax() method
+           $.ajax({
+
+             // The URL for the request
+             url: urlajax,
+
+             // The data to send (will be converted to a query string)
+             data:  { rolId: id },
+
+             // Whether this is a POST or GET request
+             method: "POST",
+
+             dataType: "json",
+
+             success: function(r) {
+               alertify.success('<?= lang('delete') ?>');
+               setTimeout("location.reload(true);", 4000);
+             },
+             error: function(r) {
+              alertify.error('<?= lang('error-del') ?>');
+               setTimeout("location.reload(true);", 3000);
+             }
+           })
+
+         },
+         function() {
+           alertify.error('Cancel');
+         }).setting('modal', true);
+     });
+
+   });
+ </script>
--- application/views/updateRol.php
+++ application/views/updateRol.php
@@ -1,121 +1,132 @@
  <!-- Begin Page Content -->
  <div class="container-fluid">
 
-  <?Php
+     <?Php
         if ($this->session->flashdata('status')) :
-    ?>
+        ?>
 
-    <div class="alert alert-success">
-        <?=$this->session->flashdata('status');?>
+         <div class="alert alert-success">
+             <?= $this->session->flashdata('status'); ?>
 
 
-    </div>
+         </div>
 
-    <?Php
+     <?Php
 
-        header('Refresh: 2');
+            header('Refresh: 2');
         endif;
-    ?>
+        ?>
 
-<!-- Page Heading -->
-<h1 class="h3 mb-2 text-gray-800"><?= lang('title-rols') ?></h1>
+     <!-- Page Heading -->
+     <h1 class="h3 mb-2 text-gray-800"><?= lang('title-rols') ?></h1>
 
-<!-- DataTales Example -->
-<div class="card shadow mb-4">
-  <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary"><?= lang('list-rols') ?></h6>
-  </div>
-  <div class="card-body">
-    <form action="#" method="post" id="frmModificar">
-        <div class="row">
-            <div class="col-md-6">
-                <label for="rolId" class="form-label">Codigo del Rol</label>
-                <input type="text" class="form-control" name="id" id="id" value="<?= $rolId ?>" disabled>
-                <input type="hidden" class="form-control" name="rolId" id="rolId" value="<?= $rolId ?>">
+     <!-- DataTales Example -->
+     <div class="card shadow mb-4">
+         <div class="card-header py-3">
+             <h6 class="m-0 font-weight-bold text-primary"><?= lang('modify-rols') ?></h6>
+         </div>
+         <div class="card-body">
+             <form action="#" method="post" id="frmModificar">
+                 <div class="row">
+                     <div class="col-md-6">
+                         <label for="rolId" class="form-label"><?= lang('rolId') ?></label>
+                         <input type="text" class="form-control" name="id" id="id" value="<?= $rolId ?>" disabled>
+                         <input type="hidden" class="form-control" name="rolId" id="rolId" value="<?= $rolId ?>">
 
-            </div>
-            <div class="col-md-6">
-                <label for="rolName" class="form-label">Rol</label>
-                <input type="text" class="form-control" name="rolName" id="rolName" value="<?= $rolName ?>">
-            </div>
-            <div class="col-md-12 mt-4">
-                <label for="rolDescription" class="form-label">Descripcion del Rol</label>
-                <textarea class="form-control" name="rolDescription" id="rolDescription"><?= $rolDescription ?></textarea>
-            </div>
-            <div class="col-md-12 mt-4">
-                <label for="activo" class="form-label">Activo</label>
-                <select name="activo" id="activo" class="form-select">
-                    <option value="1">Si</option>
-                    <option value="0">No</option>
-                </select>
-            </div>
-        </div>
-        <div class="row mt-4">
-            <div class="col-md-6 text-end">
-                <button type="submit" class="btn btn-success"><?= lang("save") ?></button>
-            </div>
-            <div class="col-md-6">
-                <button type="button"class="btn btn-danger"><?= lang("cancel") ?></button>
-            </div>
+                     </div>
+                     <div class="col-md-6">
+                         <label for="rolName" class="form-label"><?= lang('rolName') ?></label>
+                         <input type="text" class="form-control" name="rolName" id="rolName" value="<?= $rolName ?>">
+                     </div>
+                     <div class="col-md-12 mt-4">
+                         <label for="rolDescription" class="form-label"><?= lang('rolDescription') ?></label>
+                         <textarea class="form-control" name="rolDescription" id="rolDescription"><?= $rolDescription ?></textarea>
+                     </div>
+                     <div class="col-md-12 mt-4">
+                         <label for="activo" class="form-label">Activo</label>
+                         <select name="activo" id="activo" class="form-select">
+                             <option value="1" <?Php if ($activo == "1") {
+                                                    echo "selected";
+                                                } ?>>Si</option>
+                             <option value="0" <?Php if ($activo == "0") {
+                                                    echo "selected";
+                                                } ?>>No</option>
+                         </select>
+                     </div>
+                 </div>
+                 <div class="row mt-4">
+                     <div class="col-md-6 text-end">
+                         <button type="submit" class="btn btn-success"><?= lang("save") ?></button>
+                     </div>
+                     <div class="col-md-6">
+                         <button type="button" class="btn btn-danger"><?= lang("cancel") ?></button>
+                     </div>
 
-        </div>
-    </form>
-    
-  </div>
-</div>
+                 </div>
+             </form>
 
-</div>
-<!-- /.container-fluid -->
+             <div id="modificar"></div>
 
-</div>
-<!-- End of Main Content -->
+         </div>
+     </div>
 
-<script>
-    $(document).ready(function() {
-        $(".btn-danger").click(function() {
-            location.reload(); 
-        })
+ </div>
+ <!-- /.container-fluid -->
 
-        var urlbase = "<?= base_url() ?>";
-        $("#frmModificar").submit(function(event) {
-            // Prevent the form from submitting
-            event.preventDefault();
+ </div>
+ <!-- End of Main Content -->
 
+ <script>
+     $(document).ready(function() {
 
-            //creo variable con la url del ajax
-            var urlajax = urlbase + "Admin/updateModulo";
-            // Run $.ajax() here
-            // Using the core $.ajax() method
-            $.ajax({
+         var urlbase = "<?= base_url() ?>";
 
-                // The URL for the request
-                url: urlajax,
-
-                // The data to send (will be converted to a query string)
-                data: $(this).serializeArray(),
-
-                // Whether this is a POST or GET request
-                method: "POST",
-
-                dataType: "json",
+         $(".btn-danger").click(function() {
+             location.href = urlbase + 'Admin/listRols';
+             alertify.error('<?= lang('cancel-operation') ?>')
+         })
 
 
-                beforeSend: function(objeto) {                    
-                    var cargando = '<div class="alert alert-info" role="alert"><strong><?= lang("load") ?></strong><div class="spinner-border ms-auto text-primary text-end ml-5" role="status" aria-hidden="true"></div></div>';
-                    $("#modificar").html(cargando);
-                },
-                success: function(r) {
-                    $("#modificar").html('<div class="alert alert-success" role="alert"><strong><?= lang("update") ?></strong></div>');
-                    setTimeout("location.reload(true);", 3000);
-                },
-                error: function(r) {
-                    $("#modificar").html('<div class="alert alert-danger" role="alert"><strong><?= lang("error") ?></strong></div>');
-                    setTimeout("location.reload(true);", 3000);
-                }
-            })      
-        
-        });
-        
+         $("#frmModificar").submit(function(event) {
+             // Prevent the form from submitting
+             event.preventDefault();
 
-    });
-</script>
+
+             //creo variable con la url del ajax
+             var urlajax = urlbase + "Admin/updateRol";
+             // Run $.ajax() here
+             // Using the core $.ajax() method
+             $.ajax({
+
+                 // The URL for the request
+                 url: urlajax,
+
+                 // The data to send (will be converted to a query string)
+                 data: $(this).serializeArray(),
+
+                 // Whether this is a POST or GET request
+                 method: "POST",
+
+                 dataType: "json",
+
+
+                 beforeSend: function(objeto) {
+                     var cargando = '<div class="alert alert-info" role="alert"><strong><?= lang("process") ?></strong><div class="spinner-border ms-auto text-primary text-end ml-5" role="status" aria-hidden="true"></div></div>';
+                     $("#modificar").html(cargando);
+                 },
+                 success: function(r) {
+                     $("#modificar").html('<div class="alert alert-success" role="alert"><strong><?= lang("update") ?></strong></div>');
+                     setTimeout("location.reload(true);", 4000);
+                     location.href = urlbase + 'Admin/listRols';
+                 },
+                 error: function(r) {
+                     $("#modificar").html('<div class="alert alert-danger" role="alert"><strong><?= lang("error") ?></strong></div>');
+                     setTimeout("location.reload(true);", 3000);
+                 }
+             })
+
+         });
+
+
+     });
+ </script>

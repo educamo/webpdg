5da8ceb9e61092df74a88d15ed77074921341918
C Eduardo Carrasco <educamo@hotmail.com>
2022-11-04 20:11:30 -0400

    se actualizo el paquete de idioma esp

--- application/language/es/Admin_lang.php
+++ application/language/es/Admin_lang.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
- * Idioma español de Administracion
+ * Idioma español de Administración
  */
 
 ## Etiquetas Generales del sitioweb
@@ -16,7 +17,7 @@ $lang["Email"]                      = "name@example.com";
 $lang["mensajeErrlogin"]            = "debe ingresar un correo y contraseña validos";
 $lang['perfil']                     = "Perfil";
 $lang['logout']                     = "Cerrar sesión";
-$lang['desea-salir']                = "está seguro que desea salir.";
+$lang['desea-salir']                = "¿está seguro que desea salir?";
 $lang['dashboard']                  = "Dashboard";
 $lang['contents']                   = "Contenidos";
 $lang['slider']                     = "Slider";
@@ -39,36 +40,40 @@ $lang['configSocial']               = "Administrar";
 $lang['about']                      = "Nosotros";
 $lang['configContact']              = "Contacto";
 
-
 ## Etiquetas generales del panel administrativo
 $lang['modify']                     = "Modificar";
 $lang['cancel']                     = "Cancelar";
 $lang['out']                        = "Salir";
 $lang['visible']                    = "Visible";
 $lang['search']                     = "Buscar";
-$lang['cancel']                     = "Cancelar";
+$lang['cancel-operation']           = "Se Cancelo la Operación";
 $lang['save']                       = "Guardar";
+$lang['yes']                        = "Si";
+$lang['no']                         = "No";
 
 ##mensajes en formularios
-$lang['option']                     = "Seleccione una opcion...";
+$lang['option']                     = "Seleccione una opción...";
 $lang['load']                       = "Cargando... ";
+$lang['process']                    = "Procesando... ";
 $lang['update']                     = "Se actualizaron correctamente los datos";
 $lang['error']                      = "Ha ocurrido un error al actualizar los datos";
+$lang['error-del']                  = "Ha ocurrido un error al Eliminar los datos";
 $lang['errorSave']                  = "Ha ocurrido un error al Guardar los datos";
 $lang['new']                        = "Se Guardaron correctamente los datos";
-$lang['action']                     = "Accion";
+$lang['delete']                     = "Se Eliminaron correctamente los datos";
+$lang['action']                     = "Acción";
 $lang['activo']                     = "Esta Activo";
+$lang['confirm']                    = "Mensaje de Confirmación";
+$lang['confirm-message']            = "¿Esta Seguro de Eliminar el Registro?";
 
 ## etiquetas view listRols
 $lang['title-rols']                 = "Roles de Usuarios";
 $lang['list-rols']                  = "Lista de Roles";
-$lang['rolId']                      = "Id del Rol";
+$lang['modify-rols']                = "Modificar Rol";
+$lang['new-rol']                   = "Nuevo Rol";
+$lang['rolId']                      = "Código del Rol";
 $lang['rolName']                    = "Nombre del Rol";
-$lang['password']                   = "Contraseña";
-$lang['confirmarPass']              = "Confirmar Contraseña";
-$lang['confirmarPass-msj']          = "Las Contraseñas no son iguales";
-$lang['mailUser']                   = "Correo";
-$lang['rolUser']                    = "Rol";
+$lang['rolDescription']             = "Descripción del Rol";
 
 ## etiquetas view nuevoUsuario
 $lang['title-nuevoUsuario']         = "Usuarios";
@@ -81,6 +86,7 @@ $lang['confirmarPass']              = "Confirmar Contraseña";
 $lang['confirmarPass-msj']          = "Las Contraseñas no son iguales";
 $lang['mailUser']                   = "Correo";
 $lang['rolUser']                    = "Rol";
+$lang['select-option-rol']          = "Seleccione un Rol";
 
 ## etiquetas view listSocial
 $lang['title-listSocial']           = "Redes Sociales";
@@ -89,15 +95,11 @@ $lang['socialurl']                  = "Url";
 $lang['saveModify']                 = "Guardar Modificaciones";
 
 ## etiquetas view listModulos
-$lang['title-listModulos']          = "Modulos";
-$lang['viewModule']                 = "Lista de Modulos";
-$lang['moduleName']                 = "Nombre Modulo";
-$lang['moduleDescription']          = "Descripcion";
-
+$lang['title-listModulos']          = "Módulos";
+$lang['viewModule']                 = "Lista de Módulos";
+$lang['moduleName']                 = "Nombre del Modulo";
+$lang['moduleDescription']          = "Descripción";
 ## etiquetas view modificarModulo
-$lang['title-listModulos']          = "Modulos";
 $lang['searchModule']               = "Buscar Modulo";
-$lang['moduleName']                 = "Nombre Modulo";
-$lang['moduleDescription']          = "Descripcion";
 $lang['select']                     = "Debe seleccionar un modulo para modificar";
-?>
+$lang['moduleId']                   = "Código del Modulo";
